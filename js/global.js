327429cc9606fdb17aa0aac63d352ad7e6eb6c45
PedroH <pedrohp1223@gmail.com>
2025-01-24 21:52:59 -0300

    2025.01.24

--- js/global.js
+++ js/global.js
@@ -1,8 +1,16 @@
 /**
  * JavaScript do template do site.
  * É executado em todas as páginas.
- * Todo o código global fica aqui, por exemplo, gestão do usuário.
+ * Todo o código global fica aqui, por exemplo, gestão do usuário e
+ * tratamento do template.
  **/
+
+/**
+ * Inicializa o Firebase e as ferramentas Firestore e Auth
+ */
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
 
 /**
  * Quando o documento está pronto, roda o JavaScript
@@ -10,7 +18,7 @@
 window.onload = () => { // Isso é uma "arrow function"
 
     /**
-     * Altera o <title> padrão da página.
+     * Altera o <title> padrão da página atual.
      **/
     document.title = site.nome;
 
@@ -19,13 +27,21 @@ window.onload = () => { // Isso é uma "arrow function"
 
     /**
      * Obtém o ano da data atual e atualiza a licensa do site.
+     * O elemento span#footerAno está definido em funcoes.js → template()
      **/
     let agora = new Date();
     let ano = agora.getFullYear();
-    console.log(_('#footerAno'))
     if (ano > site.ano)
+        /**
+         * Se o ano atual é maior que o da fundação do site, 
+         * mostra o ano de fundação e o atual.
+         **/
         _('#footerAno').innerHTML = `${site.ano} ${ano}`;
     else
-        _('#footerAno').innerHTML = ano;
+        /**
+         * Se o ano de fundação é o atual,
+         * mostra o ano de fundação.
+         **/
+        _('#footerAno').innerHTML = site.ano;
 
 }
